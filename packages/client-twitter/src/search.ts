639ebfeb8b36fa6e2cf0bb8804d5594385ad2aef
samuel-videau <samuel.videau@efrei.net>
2024-12-13 02:57:50 +1100

    agoria intern character

--- packages/client-twitter/src/search.ts
+++ packages/client-twitter/src/search.ts
@@ -58,7 +58,7 @@ export class TwitterSearchClient extends ClientBase {
         this.engageWithSearchTerms();
         setTimeout(
             () => this.engageWithSearchTermsLoop(),
-            (Math.floor(Math.random() * (10 - 1 + 1)) + 1) * 60 * 1000
+            (Math.floor(Math.random() * (120 - 60 + 1)) + 60) * 60 * 1000
         );
     }
 
